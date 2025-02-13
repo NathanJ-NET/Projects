b904bf80531e8e360af51527d8a36aa4a1c32d73
NathanJ-NET <nzjordan1@my.waketech.edu>
2025-02-13 10:48:46 -0500

    Changed the code a little bit, dealing with errors converting file to an executable

--- Conversions.py
+++ Conversions.py
@@ -1,6 +1,6 @@
 def Conversions():
-    while True: 
-        UserPrompt1 = input("Welcome to the useless conversion killer! \nWould you like to convert decimal to binary (1), binary to decimal (2), hexadecimal to binary (3), \nbinary to hexadecimal (4), decimal to hexadecimal (5), or hexadecimal to decimal (6)?")
+    while True:
+        UserPrompt1 = input("Welcome to the useless conversion killer! \nWould you like to convert decimal to binary (1), binary to decimal (2), hexadecimal to binary (3), \nbinary to hexadecimal (4), decimal to hexadecimal (5), or hexadecimal to decimal (6)? ")
 
         if UserPrompt1 == '1': 
             decimal_num = input("Enter your decimal value here: ")
@@ -24,16 +24,18 @@ def Conversions():
             except ValueError:
                 print("I asked for a valid binary number, not whatever that is. Try again.")
                 continue  
+
         elif UserPrompt1 == '3':
             hexa_num = input("Enter your hexadecimal value here: ")
             print(f"You want {hexa_num}? Sounds good! Doing calculations and stuff...")
 
             try:
-                binary_num = bin(int(hexa_num, 16)) [2:]
+                binary_num = bin(int(hexa_num, 16))[2:]
                 print(f"Here's your hexadecimal value {hexa_num} as a binary value: {binary_num}.")
             except ValueError:
                 print("I asked for a valid hexadecimal number, not whatever that is. Try again.")
                 continue  
+
         elif UserPrompt1 == '4':
             binary_num = input("Enter your binary value here: ")
             print(f"You want {binary_num}? Sounds good! Doing calculations and stuff...")   
@@ -44,10 +46,10 @@ def Conversions():
             except ValueError:
                 print("I asked for a valid binary number, not whatever that is. Try again.")
                 continue 
+
         elif UserPrompt1 == '5':
             decimal_num = input("Enter your decimal value here: ")
             print(f"You want {decimal_num}? Sounds good! Doing calculations and stuff...")
-
 
             try:
                 decimal_num = int(decimal_num)
@@ -56,6 +58,7 @@ def Conversions():
             except ValueError:
                 print("I asked for a valid integer, not whatever that is. Try again.")
                 continue 
+
         elif UserPrompt1 == '6':
             hexa_num = input("Enter your hexadecimal value here: ")
             print(f"You want {hexa_num}? Sounds good, doing calculations and stuff...")
@@ -66,6 +69,7 @@ def Conversions():
             except ValueError:
                 print("I asked for a valid hexadecimal number, not whatever that is. Try again.")
                 continue
+
         else:
             print("I asked for a 1, 2, 3, 4, 5 or 6, not whatever the hell you entered. Try again.")
             continue  
@@ -75,7 +79,8 @@ def Conversions():
             if continue_or_exit.lower() == 'y':
                 break
             elif continue_or_exit.lower() == 'n':
-                return 
+                print("Goodbye!")
+                return  
             else:
                 print("I asked for a y or an n, not whatever that is. Try again.")
 
